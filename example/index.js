70a4ee97a406950f1fb913b922c5bc93aa601f47
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2021-12-07 13:44:22 +0100

    fix rngh package errors

--- example/index.js
+++ example/index.js
@@ -6,7 +6,4 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
-console.log(AppRegistry.getAppKeys())
-console.log('Dreams')
-
 AppRegistry.registerComponent(appName, () => App)
