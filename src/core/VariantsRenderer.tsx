b43ca7ece3ab20db3f2bf44cdd533d68ec2a466a
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2022-01-07 13:47:08 +0100

    add onPress notification removal

--- src/core/VariantsRenderer.tsx
+++ src/core/VariantsRenderer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { EmitParam } from './createNotifications'
 import type { NotificationsConfig, Variant } from '../types'
+import { useNotificationController } from '../hooks/useNotificationController'
 
 type Props = {
   config: NotificationsConfig<any>
@@ -12,9 +13,14 @@ export const VariantsRenderer = (props: Props) => {
 
   const Component = variant.component
 
+  const { remove } = useNotificationController(props.notificationConfig)
+
   return (
     <>
-      <Component {...{ ...variant.defaultProps, ...props.notificationConfig.params }} />
+      <Component
+        {...{ ...variant.defaultProps, ...props.notificationConfig.params }}
+        onPress={remove}
+      />
     </>
   )
 }
