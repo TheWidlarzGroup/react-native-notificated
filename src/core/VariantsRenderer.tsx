30663ddf2c07b25094624d279a96f3b093002067
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2022-01-10 20:44:21 +0100

    remove useNotificationController usage

--- src/core/VariantsRenderer.tsx
+++ src/core/VariantsRenderer.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import type { EmitParam } from './createNotifications'
 import type { NotificationsConfig, Variant } from '../types'
-import { useNotificationController } from '../hooks/useNotificationController'
 
 type Props = {
   config: NotificationsConfig<any>
@@ -13,14 +12,9 @@ export const VariantsRenderer = (props: Props) => {
 
   const Component = variant.component
 
-  const { remove } = useNotificationController(props.notificationConfig)
-
   return (
     <>
-      <Component
-        {...{ ...variant.defaultProps, ...props.notificationConfig.params }}
-        onPress={remove}
-      />
+      <Component {...{ ...variant.defaultProps, ...props.notificationConfig.params }} />
     </>
   )
 }
