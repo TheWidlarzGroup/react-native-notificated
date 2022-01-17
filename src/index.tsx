ebb9445d26f94673f39e9c7a23d66483ecbf8888
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-01-17 21:16:30 +0100

    Update exports from library

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 import { createNotifications } from './core/createNotifications'
+import { ZoomInAndOut, SlideInLeftSlideOutRight } from './defaultConfig/defaultAnimationConfig'
+import { defaultVariants } from './defaultConfig/defaultConfig'
 
 export { createNotifications }
+
+export { defaultVariants }
+
+export { ZoomInAndOut, SlideInLeftSlideOutRight }
