f5edfccd1d6ef2e079be63fc1f40bf96c2ad6608
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-01-17 21:15:05 +0100

    Add generateAnimationConfig func

---
+++ src/core/generateAnimationConfig.ts
@@ -0,0 +1,5 @@
+import type { CustomAnimationConfig } from 'src/types/animations'
+
+export const generateAnimationConfig = (config: CustomAnimationConfig): CustomAnimationConfig => {
+  return config
+}
