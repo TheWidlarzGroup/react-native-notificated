dbd308b6ad87b81437b8b1c39f91d8f03ed6b617
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-01-18 22:04:01 +0100

    Update exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { createNotifications } from './core/createNotifications'
-import { ZoomInAndOut, SlideInLeftSlideOutRight } from './defaultConfig/defaultAnimationConfig'
+import { ZoomInDownZoomOutUp, SlideInLeftSlideOutRight, ZoomInDownZoomOutDown, RotateInRotateOut, RotateZIn } from './defaultConfig/defaultAnimationConfig'
 import { defaultVariants } from './defaultConfig/defaultConfig'
 
 export { createNotifications }
 
 export { defaultVariants }
 
-export { ZoomInAndOut, SlideInLeftSlideOutRight }
+export { ZoomInDownZoomOutUp, SlideInLeftSlideOutRight, ZoomInDownZoomOutDown, RotateInRotateOut, RotateZIn }
