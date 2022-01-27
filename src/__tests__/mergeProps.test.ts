84088fa0050e0b8d261dd1941a2f008fe86220bb
Piter Bedlechowicz <piter@thewidlarzgroup.com>
2022-01-27 13:49:11 +0100

    Add tests structure

--- src/__tests__/mergeProps.test.ts
+++ src/__tests__/mergeProps.test.ts
@@ -1,45 +1,53 @@
 import { mergeProps } from '../defaultConfig/mergeProps'
+import type { NotificationProps } from '../types'
+import type { NotificationStyleConfig } from '../defaultConfig/types'
 
-test('get proper props for component', () => {
-  const props = {
-    description: 'test description props',
-    descriptionColor: 'rgb(0, 0, 50)',
-    borderRadius: 5,
-    borderType: 'no-border',
-  }
+describe('props merger tests', function () {
+  it.todo('should merge props with props as main source')
+  it.todo('should merge props with variant config as main source')
+  it.todo('should merge props with global config as main source')
 
-  const globalConfig = {
-    description: 'test description global',
-    descriptionSize: 5,
-    descriptionColor: 'rgb(0, 100, 0)',
-    borderRadius: 5,
-  }
+  it('should merge props with mixed sources', () => {
+    const props: NotificationProps = {
+      description: 'test description props',
+      style: {
+        descriptionColor: 'rgb(0, 0, 50)',
+        borderRadius: 5,
+        borderType: 'no-border',
+      },
+    }
 
-  const notificationTypeConfig = {
-    description: 'test description',
-    titleSize: 23,
-    titleColor: '#fff',
-    descriptionSize: 10,
-    descriptionColor: 'rgb(0, 0, 0)',
-    borderRadius: 5,
-  }
+    const globalConfig: NotificationStyleConfig = {
+      descriptionSize: 5,
+      descriptionColor: 'rgb(0, 100, 0)',
+      borderRadius: 5,
+    }
 
-  expect(mergeProps(props, 'error', true, globalConfig, notificationTypeConfig)).toStrictEqual({
-    title: '',
-    description: 'test description props',
-    theme: 'dark',
-    titleSize: 23,
-    titleColor: '#fff',
-    descriptionColor: 'rgb(0, 0, 0)',
-    descriptionSize: 10,
-    bgColor: undefined,
-    borderWidth: 1,
-    multiline: undefined,
-    defaultIconType: undefined,
-    borderType: 'border',
-    borderRadius: 5,
-    accentColor: '#FC6060',
-    leftIconSource: require('../../src/assets/images/error-color.png'),
-    onPress: undefined,
+    const notificationTypeConfig: NotificationStyleConfig = {
+      titleSize: 23,
+      titleColor: '#fff',
+      descriptionSize: 10,
+      descriptionColor: 'rgb(0, 0, 0)',
+      borderRadius: 5,
+    }
+
+    expect(mergeProps(props, 'error', true, globalConfig, notificationTypeConfig)).toStrictEqual({
+      title: '',
+      description: 'test description props',
+      theme: 'dark',
+      titleSize: 23,
+      titleColor: '#fff',
+      descriptionColor: 'rgb(0, 0, 50)',
+      descriptionSize: 10,
+      bgColor: undefined,
+      borderWidth: 1,
+      multiline: undefined,
+      defaultIconType: undefined,
+      borderType: 'no-border',
+      borderRadius: 5,
+      accentColor: '#FC6060',
+      leftIconSource: expect.any(Object),
+      onPress: undefined,
+    })
   })
 })
