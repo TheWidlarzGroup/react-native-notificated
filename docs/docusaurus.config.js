c13fff471005dca07e853691b7c26256ef223caf
mateki0 <mateki0@interia.pl>
2022-03-17 17:54:20 +0100

    changed Git_USER

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 const config = {
   title: 'RN Notification',
   tagline: 'React Native Notifications',
-  url: 'https://thewidlarzgroup.github.io/react-native-notification/',
+  url: 'https://thewidlarzgroup.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
