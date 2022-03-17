0b52b252422ae17e83a1d4cdd108ff45f878a437
mateki0 <mateki0@interia.pl>
2022-03-17 17:31:13 +0100

    add git_user to the deploy workflow

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,14 +7,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'RN Notification',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  tagline: 'React Native Notifications',
+  url: 'https://thewidlarzgroup.github.io/react-native-notification/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'thewidlarzgroup', // Usually your GitHub org/user name.
+  projectName: 'react-native-notification', // Usually your repo name.
 
   presets: [
     [
