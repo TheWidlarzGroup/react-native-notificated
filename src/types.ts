bbbbeef89a29b99c99a91148bc5e7c22864a49b2
Konrad Gauza <konradgauza@gmail.com>
2022-05-22 20:09:19 +0200

    fix conflicts in the src./types.ts

--- src/types.ts
+++ src/types.ts
@@ -2,24 +2,15 @@ import type { CustomAnimationConfig } from './types/animations'
 import type { DefaultLayoutConfig, DefaultVariants } from './defaultConfig/types'
 import type { NotificationPosition } from './types/config'
 import type { GestureConfig } from './types/gestures'
-<<<<<<< HEAD
-import type { ComponentProps, FC } from 'react'
-
-export type RequiredProps<T extends Variant<FC>> = ComponentProps<T['component']>
-=======
 import type { ComponentProps, VFC } from 'react'
 
 export type RequiredProps<T extends Variant<VFC<any>>> = ComponentProps<T['component']>
->>>>>>> origin
 
 export type Variant<T> = {
   component: T
   config?: Partial<Omit<NotificationConfigBase, 'isNotch'>>
 }
 
-<<<<<<< HEAD
-export type VariantsMap = Readonly<Record<string, Variant<FC<any>>>>
-=======
 export type VariantsMap = Record<string, Variant<VFC<any>>>
 
 export interface CustomVariants {}
@@ -27,7 +18,6 @@ export interface CustomVariants {}
 export type Variants = CustomVariants[keyof CustomVariants] extends never
   ? DefaultVariants
   : CustomVariants
->>>>>>> origin
 
 export type NotificationConfigBase = {
   duration: number
@@ -43,3 +33,4 @@ export type NotificationsConfig<Variants> = {
   DefaultLayoutConfig
 
 export type { CustomAnimationConfig }
+
