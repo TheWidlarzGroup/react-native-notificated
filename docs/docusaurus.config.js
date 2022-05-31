31afcaf63126cbcfcd9c77a9fa016154b2adc027
ssavi.ict <ssavi.ict@gmail.com>
2022-05-31 08:34:09 +0600

    suggested changes

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -59,8 +59,7 @@ const config = {
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            // href: 'https://github.com/facebook/docusaurus',
-			href: 'https://github.com/TheWidlarzGroup/react-native-notificated',
+            href: 'https://github.com/TheWidlarzGroup/react-native-notificated',
             label: 'GitHub',
             position: 'right',
           },
