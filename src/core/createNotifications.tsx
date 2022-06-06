8c40637cb33e71a2802ab027ed1a7081221b61a3
mateki0 <mateki0@interia.pl>
2022-06-06 09:22:57 +0200

    make config prop optional in createNotifications

--- src/core/createNotifications.tsx
+++ src/core/createNotifications.tsx
@@ -8,8 +8,7 @@ import { NotificationsRenderer } from './renderers/NotificationsRenderer'
 import { NotificationContext } from './hooks/useNotificationConfig'
 
 export const createNotifications = <Variants extends VariantsMap = DefaultVariants>(
-  config: Partial<NotificationsConfig<Variants>>
-  // | Partial<NotificationsConfig<_DefaultVariants>> = {}
+  config?: Partial<NotificationsConfig<Variants>>
 ): {
   useNotifications: () => {
     modify: Modify
