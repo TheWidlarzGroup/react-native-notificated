46b4d103073dcb341b3cf7b5e19b163d65c8e326
mateki0 <mateki0@interia.pl>
2022-06-21 14:26:32 +0200

    created new config

--- src/core/utils/generateAnimationConfig.ts
+++ src/core/utils/generateAnimationConfig.ts
@@ -1,5 +1,39 @@
-import type { CustomAnimationConfig } from '../../types/animations'
+import type { AnimationTypeConfig, CustomAnimationConfig } from '../../types/animations'
 
-export const generateAnimationConfig = (config: CustomAnimationConfig): CustomAnimationConfig => {
-  return config
+class AnimationBuilder {
+  animationConfigIn: AnimationTypeConfig
+  animationConfigOut?: AnimationTypeConfig
+  transitionInStyles: any
+  transitionOutStyles: any
+
+  stylesFuncsQueue: any[] = []
+
+  constructor(config: CustomAnimationConfig) {
+    this.animationConfigIn = config.animationConfigIn
+    this.animationConfigOut = config.animationConfigOut
+    this.transitionInStyles = config.transitionInStyles
+    this.transitionOutStyles = config.transitionOutStyles
+  }
+
+  add(configToPipe: AnimationBuilder) {
+    this.animationConfigIn = configToPipe.animationConfigIn
+    this.animationConfigOut = configToPipe.animationConfigOut
+
+    this.stylesFuncsQueue.push(configToPipe.transitionInStyles)
+  }
+}
+
+export const MyNewConfig = new AnimationBuilder({
+  animationConfigIn: { type: 'timing', config: {} },
+  animationConfigOut: { type: 'timing', config: {} },
+  transitionInStyles: (progress) => {
+    return { opacity: progress.value }
+  },
+  transitionOutStyles: (progress) => {
+    return { opacity: progress.value }
+  },
+})
+
+export const generateAnimationConfig = (_config: CustomAnimationConfig) => {
+  return MyNewConfig
 }
