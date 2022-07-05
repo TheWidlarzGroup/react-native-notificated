22ebaf249a952eb0c0f40590433e0ba875e32818
Magda <jaskowska.magda@gmail.com>
2022-07-05 16:11:00 +0100

    config.ts cleanup

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,25 +1,16 @@
 //do wyrzucenia
-import { Dimensions, Platform } from 'react-native'
+import { Platform } from 'react-native'
 
 const isAndroid = Platform.OS === 'android'
 const maxLongPressDragDistance = 300
-const { width, height } = Dimensions.get('window')
 const notificationSideMargin = 14
-const notificationWidth = width - notificationSideMargin * 2
-const initialOffsetX = -(notificationWidth + 2 * notificationSideMargin)
 const initialOffsetY = -300
-const targetOffsetX = width
 const targetOffsetY = true ? 50 : 10
 
 export const Constants = {
   maxLongPressDragDistance,
   notificationSideMargin,
-  notificationWidth,
-  initialOffsetX,
   initialOffsetY,
-  targetOffsetX,
   targetOffsetY,
   isAndroid,
-  height,
-  width,
 }
