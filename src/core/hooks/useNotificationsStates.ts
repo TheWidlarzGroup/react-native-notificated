45bfa59524268413c979c2028e458571ab7df023
Magda <jaskowska.magda@gmail.com>
2022-07-07 13:00:38 +0100

    init logic to handle notch

--- src/core/hooks/useNotificationsStates.ts
+++ src/core/hooks/useNotificationsStates.ts
@@ -1,26 +1,38 @@
-import { useWindowDimensions } from 'react-native'
+import { useEffect, useReducer, useRef, useState } from 'react'
+import { useWindowDimensions, NativeModules, Platform, StatusBar, Alert } from 'react-native'
 import { useNotificationConfig } from './useNotificationConfig'
-import { useReducer, useRef, useState } from 'react'
 import { getTopOffset, mergeConfigs } from '../utils/pickers'
 import { queueReducer } from '../utils/queueReducer'
 
 export const useNotificationsStates = () => {
+  const { StatusBarManager } = NativeModules
   const panHandlerRef = useRef(null)
   const longPressHandlerRef = useRef(null)
   const { height: windowHeight, width: windowWidth } = useWindowDimensions()
   const globalConfig = useNotificationConfig()
   const [notificationsQueue, dispatch] = useReducer(queueReducer, [])
   const [notificationHeight, setNotificationHeight] = useState(0)
+  const [barHeight, setBarHeight] = useState(0)
 
   const isPortaitMode = windowHeight > windowWidth
   const notificationEvent = notificationsQueue[0]
   const config = mergeConfigs(globalConfig, notificationEvent)
+
+  useEffect(() => {
+    Alert.alert('bar Height:', `${barHeight}`)
+  }, [barHeight])
+
+  useEffect(() => {
+    if (Platform.OS !== 'ios') return setBarHeight(StatusBar.currentHeight ?? 0)
+    setBarHeight(isPortaitMode && StatusBarManager.HEIGHT !== 0 ? StatusBarManager.HEIGHT : 50)
+  }, [StatusBarManager.HEIGHT, isPortaitMode])
 
   const topOffset = getTopOffset({
     globalConfig: config,
     notificationHeight,
     isPortaitMode,
     windowHeight,
+    statusBarHeight: barHeight,
   })
 
   return {
