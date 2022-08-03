f8ca40af50cc3921724cc50e69953e17ed3c71e3
Jarmy123 <romanjarmuz@gmail.com>
2022-08-03 10:41:46 +0200

    Test change in expo project

--- example-expo/src/screens/DefaultExamples.tsx
+++ example-expo/src/screens/DefaultExamples.tsx
@@ -12,7 +12,7 @@ export const DefaultExamples = () => {
           variant="success"
           onPress={() =>
             notify('success', {
-              params: { title: 'Hello!', description: 'Some text goes here...' },
+              params: { title: 'Helloo!', description: 'Some text goes here...' },
             })
           }
         />
