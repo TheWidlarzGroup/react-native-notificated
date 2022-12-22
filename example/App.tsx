acccfe95f7a2c2b1b89a3b178650c45cf2d8fe67
mateki0 <mateki0@interia.pl>
2022-12-22 13:58:22 +0100

    fix: android build, removed firebase packages and code

--- example/App.tsx
+++ example/App.tsx
@@ -1,31 +1,12 @@
 import 'react-native-gesture-handler'
-import React, { useEffect } from 'react'
-import messaging from '@react-native-firebase/messaging'
+import React from 'react'
 import { AppNavigator } from './src/navigation'
-import { useNotifications } from 'react-native-notificated'
 import { NavigationContainer } from '@react-navigation/native'
 
-const App = () => {
-  const { notify } = useNotifications()
-
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      notify('info', {
-        params: {
-          title: remoteMessage.notification?.title || '',
-          description: remoteMessage.notification?.body,
-        },
-      })
-    })
-
-    return unsubscribe
-  }, [notify])
-
-  return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
-  )
-}
+const App = () => (
+  <NavigationContainer>
+    <AppNavigator />
+  </NavigationContainer>
+)
 
 export default App
