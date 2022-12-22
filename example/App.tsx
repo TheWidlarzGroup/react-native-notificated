33ef18ba89c904352a82366a38fc124b151d0edf
Mateusz Bętka <37906388+mateki0@users.noreply.github.com>
2022-12-22 09:44:45 +0100

    Revert "feat: handle foreground push notifications (#161)"
    This reverts commit 6029a501cc9ad73c4f37bf6a1c7da611a65231f4.

--- example/App.tsx
+++ example/App.tsx
@@ -1,26 +1,9 @@
 import 'react-native-gesture-handler'
-import React, { useEffect } from 'react'
-import messaging from '@react-native-firebase/messaging'
-import { AppNavigator } from './src/navigation'
-import { useNotifications } from 'react-native-notificated'
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { AppNavigator } from './src/navigation'
 
 const App = () => {
-  const { notify } = useNotifications()
-
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      notify('info', {
-        params: {
-          title: remoteMessage.notification?.title || '',
-          description: remoteMessage.notification?.body,
-        },
-      })
-    })
-
-    return unsubscribe
-  }, [notify])
-
   return (
     <NavigationContainer>
       <AppNavigator />
