d42ea6cac0c3a22406e62b8aa3e87bce04e352f1
Mateusz Bętka <37906388+mateki0@users.noreply.github.com>
2023-03-08 10:12:38 +0100

    fix: changed the way of getting status bar height on ios (#202)

--- src/core/hooks/useStatusBarHeightDetector.ts
+++ src/core/hooks/useStatusBarHeightDetector.ts
@@ -14,9 +14,9 @@ export const useStatusBarHeightDetector = ({ isPortraitMode }: Props) => {
   useEffect(() => {
     if (Platform.OS !== 'ios') return setBarHeight(StatusBar.currentHeight ?? 0)
     // handling edge case when app is opened in landscape mode and barHeight = 0
-    const StatusBarManagerModule = NativeStatusBarManager || StatusBarManager
+    const StatusBarManagerModule = NativeStatusBarManager?.default || StatusBarManager
 
-    StatusBarManagerModule.getHeight(({ height }: { height: number }) =>
+    StatusBarManagerModule?.getHeight(({ height }: { height: number }) =>
       setBarHeight(isPortraitMode && height !== 0 ? height : 50)
     )
   }, [StatusBarManager, isPortraitMode])
