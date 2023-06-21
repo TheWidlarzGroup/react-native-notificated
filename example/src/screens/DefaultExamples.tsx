6e474322d5faa8c4db044c28b80cc76055b40cf6
mateki0 <mateki0@interia.pl>
2023-06-21 15:13:22 +0200

    feat: add react-native-modal to be able to test

--- example/src/screens/DefaultExamples.tsx
+++ example/src/screens/DefaultExamples.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native'
+import Modal from 'react-native-modal'
 import { createNotifications } from 'react-native-notificated'
 import { SuccessButton } from '../components/basicExamples/SuccessButton'
 import { ErrorButton } from '../components/basicExamples/ErrorButton'
@@ -21,6 +22,7 @@ const { useNotifications, NotificationsProvider } = createNotifications({
 export const DefaultExamples = () => {
   const { notify, remove, modify } = useNotifications()
   const [id, setId] = useState('')
+  const [isVisible, setIsVisible] = useState(false)
 
   return (
     <SafeAreaView style={styles.container}>
@@ -86,7 +88,33 @@ export const DefaultExamples = () => {
         }
       />
 
-      <RemoveButton onPress={() => remove(id)} />
+      <RemoveButton onPress={() => setIsVisible(true)} />
+
+      <Modal
+        isVisible={isVisible}
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingHorizontal: 20,
+          zIndex: 2,
+        }}>
+        {/* <NotificationsProvider /> */}
+        <WarningButton
+          onPress={() =>
+            notify(
+              'warning',
+              {
+                params: {
+                  description: 'This is where the toast text goes',
+                  title: 'Warning',
+                },
+              },
+              'customId'
+            )
+          }
+        />
+      </Modal>
     </SafeAreaView>
   )
 }
