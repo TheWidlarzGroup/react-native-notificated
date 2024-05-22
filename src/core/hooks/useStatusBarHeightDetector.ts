42ad3b73f3f33ed645423d40e4086f4bc1039070
Daniel Grychtoł <grychtol.daniel@gmail.com>
2024-05-22 15:26:53 +0200

    fix: split useStatusBarHeightDetector hook into web and native flavours

--- src/core/hooks/useStatusBarHeightDetector.ts
+++ src/core/hooks/useStatusBarHeightDetector.ts
@@ -1,27 +1,9 @@
-import { useEffect, useState } from 'react'
-import { NativeModules, Platform, StatusBar } from 'react-native'
-
 type Props = {
   isPortraitMode: boolean
 }
 
-const NativeStatusBarManager = require('react-native/Libraries/Components/StatusBar/NativeStatusBarManagerIOS')
-
-export const useStatusBarHeightDetector = ({ isPortraitMode }: Props) => {
-  const { StatusBarManager } = NativeModules
-  const [barHeight, setBarHeight] = useState(0)
-
-  useEffect(() => {
-    if (Platform.OS !== 'ios') return setBarHeight(StatusBar.currentHeight ?? 0)
-    // handling edge case when app is opened in landscape mode and barHeight = 0
-    const StatusBarManagerModule = NativeStatusBarManager?.default || StatusBarManager
-
-    StatusBarManagerModule?.getHeight(({ height }: { height: number }) =>
-      setBarHeight(isPortraitMode && height !== 0 ? height : 50)
-    )
-  }, [StatusBarManager, isPortraitMode])
-
+export const useStatusBarHeightDetector = ({ isPortraitMode: _ }: Props) => {
   return {
-    statusBarHeight: barHeight,
+    statusBarHeight: 0,
   }
 }
